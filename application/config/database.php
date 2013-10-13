51e06ee6ef089f288b75f215c9018af6e2d58882
JulienA <ju.aubert87@gmail.com>
2013-10-13 06:52:46 -0700

    elastic GoGo

--- application/config/database.php
+++ application/config/database.php
@@ -48,9 +48,9 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'localhost';
+$db['default']['hostname'] = '192.168.157.130';
 $db['default']['username'] = 'root';
-$db['default']['password'] = '';
+$db['default']['password'] = 'root';
 $db['default']['database'] = 'pespo_db2';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
